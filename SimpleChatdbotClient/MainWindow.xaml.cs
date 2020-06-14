486896f59422d05c6defb74615a6fb638acd22b5
Samyunwei <259375702@qq.com>
2020-06-15 02:06:44 +0800

    添加项目文件。

---
+++ SimpleChatdbotClient/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Grpc.Core;
+using Simplechatbot;
+using System.Threading.Tasks;
+
+namespace SimpleChatdbotClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private SimpleChatBotClientImp client;
+        private IniFile mConfig;
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var addr = mConfig.IniReadValue("Server", "ADDR").ToString();
+            var channel = new Channel(addr, ChannelCredentials.Insecure);
+            var client = new SimpleChatBotClientImp(new SimpleChatBotServer.SimpleChatBotServerClient(channel));
+            var message = textBox_message.Text;
+            var rep = makeResponse("自己", message);
+            textBox_history.AppendText(rep);
+            textBox_message.Text = ""; 
+            var rep_data = client.Chat(message);
+
+            rep = makeResponse("聊天机器人", rep_data);
+            textBox_history.AppendText(rep);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            mConfig = new IniFile("./cfg.ini");
+        }
+
+        string makeResponse(string speaker,string data)
+        {
+            return string.Format("{0} ：\n       {1}\n", speaker, data);
+            
+        }
+    }
+}
---
+++ SimpleChatdbotClient/SimpleChatBotClientImp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Simplechatbot;
+using Grpc.Core;
+
+namespace SimpleChatdbotClient
+{
+    class SimpleChatBotClientImp
+    {
+        private Simplechatbot.SimpleChatBotServer.SimpleChatBotServerClient client;
+        private int id = 0;
+
+        public SimpleChatBotClientImp(SimpleChatBotServer.SimpleChatBotServerClient client)
+        {
+            this.client = client;
+        }
+
+        private void Log(string s, params object[] args)
+        {
+            Console.WriteLine(string.Format(s, args));
+        }
+
+        private void Log(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public async Task<string> ChatAsync(string data)
+        {
+            try
+            {
+                var req = new Simplechatbot.ChatRequest();
+                id++;
+                req.Ids = $"id";
+                req.Data = data;
+                using (var call = client.ChatAsync(req))
+                {
+                    var responseReaderTask = Task.Run(async () =>
+                    {
+                        await call.ResponseAsync;
+                        var rep = call.ResponseAsync.Result;
+                        Log("Recv Rep ids:{0},result:{1}", rep.Ids, rep.Data);
+                        return rep.Data;
+                    });
+                    return await responseReaderTask;
+                }
+            }
+            catch (RpcException e)
+            {
+                Log(" Chat failed {0}", e);
+                throw;
+            }
+
+        }
+
+
+        public string Chat(string data)
+        {
+            try
+            {
+                var req = new Simplechatbot.ChatRequest();
+                id++;
+                req.Ids = $"id";
+                req.Data = data;
+                var rep = client.Chat(req);
+                return rep.Data;
+            }
+            catch (RpcException e)
+            {
+                Log(" Chat failed {0}", e);
+                throw;
+            }
+        }
+    }
+
+}
